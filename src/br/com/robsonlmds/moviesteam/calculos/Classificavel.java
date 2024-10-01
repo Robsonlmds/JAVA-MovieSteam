afdc8fa3c7bca49090903f797c803266abf711c6
Robson Lucas Messias <98341530+Robsonlmds@users.noreply.github.com>
2024-10-01 15:55:04 -0300

    Delete src/br/com/robsonlmds/moviesteam directory

--- src/br/com/robsonlmds/moviesteam/calculos/Classificavel.java
+++
@@ -1,5 +0,0 @@
-package br.com.robsonlmds.moviesteam.calculos;
-
-public interface Classificavel {
-    int getClassificacao();
-}
--- src/br/com/robsonlmds/moviesteam/modelos/TituloOmdb.java
+++
@@ -1,4 +0,0 @@
-package br.com.robsonlmds.moviesteam.modelos;
-
-public record TituloOmdb(String title, String year, String runtime) {
-}
